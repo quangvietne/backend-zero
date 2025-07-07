acc308ac01f12b11aa02e9957535386c128fbad8
Nguyen Quang Viet <quangviet098@gmail.com>
2025-07-07 10:40:52 +0700

    update nav

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,16 +2,7 @@ const connection = require("../config/database");
 
 const getHomePage = (req, res) => {
   // process data -> gọi models
-  let user = [];
-  connection.query("SELECT * FROM Users", function (err, results, fields) {
-    if (err) {
-      console.error("Query failed:", err); // ← CỰC KỲ QUAN TRỌNG
-      return;
-    }
-    user = results;
-    console.log(">>> results =", results);
-    res.send(JSON.stringify(user)); // convert sang string mới in ra đc
-  });
+  return res.render("home.ejs");
 };
 
 const getABC = (req, res) => {
